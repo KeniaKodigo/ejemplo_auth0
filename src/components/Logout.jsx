3ec92618745b4e1322562581e8c76aacb69ad40c
KenyaPaiz <paizkenia5@gmail.com>
2023-01-27 12:06:49 -0600

    primer commit

---
+++ src/components/Logout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function Login(){
+    const { logout } = useAuth0();
+
+    return <button onClick={ () => logout( { logoutParams: { returnTo: window.location.origin } }) }>Cerrar Sesion</button>
+}
+
+export default Login;
